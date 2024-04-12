acbae75c1ab7eb82c0b3a4d3e341ee4c2a51fe10
Carlos0101py <73728755+Carlos0101py@users.noreply.github.com>
2024-04-11 22:28:45 -0300

    feat: Configurando o run.py

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -3,9 +3,14 @@ from flask_cors import CORS
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project_data.db'
+load_dotenv()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- run.py
+++ run.py
@@ -1,4 +1,16 @@
 from app import app
+import os
+from dotenv import load_dotenv
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    
+    load_dotenv()
+    port=os.getenv('PORT')
+    production=os.getenv('PRODUCTION')
+
+    debug = True
+
+    if production == 'False':
+        debug = False
+
+    app.run(port=port, debug=debug, host='0.0.0.0')
