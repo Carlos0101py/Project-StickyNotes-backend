25e013a7e13873535cc9d12362c38606d181e2a1
Carlos0101py <73728755+Carlos0101py@users.noreply.github.com>
2024-04-11 22:44:39 -0300

    fix: acrecentando a variavel app

--- run.py
+++ run.py
@@ -2,15 +2,18 @@ from app import app
 import os
 from dotenv import load_dotenv
 
+
+load_dotenv()
+app=app
+
 if __name__ == '__main__':
     
-    load_dotenv()
     port=os.getenv('PORT')
     production=os.getenv('PRODUCTION')
 
     debug = True
 
-    if production == 'False':
+    if production == 'True':
         debug = False
 
     app.run(port=port, debug=debug, host='0.0.0.0')
